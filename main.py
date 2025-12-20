58f7747c1eccddefd136b4009e99e45e1549259d
Cruzer28 <cruzer282@gmail.com>
2025-12-20 00:46:57 -0600

    Return sorted list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from stats import get_word_count, get_character_count
+from stats import get_word_count, get_character_count, sort_dict
 
+# Retrieve text file and return contents
 def get_book_text(file):
     with open(file) as f:    
         file_contents = f.read()
@@ -9,13 +10,19 @@ def get_book_text(file):
 def main():
     text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(text)
-    character_count = get_character_count(text)
-
+    unsorted_character_count = get_character_count(text)
+    character_count = sort_dict(unsorted_character_count)
+    
     print(f"Found {word_count} total words")
 
-    for char in character_count:
-        number = character_count[char]
-        print(f"'{char}': {number}")
+    # Print out list and number of each character from text
+    for line in character_count:
+        char = line["char"]
+        num = line["num"]
+
+        # Include only alphabetical characters
+        if char.isalpha():
+            print(f"{char}: {num}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -19,3 +19,17 @@ def get_character_count(text):
             character_count[char] +=1
     
     return character_count
+
+def sort_on(items):
+    return items["num"]
+
+# Turn dictionary into sorted list of dictionaries
+def sort_dict(dict):
+
+    sorted_list = []
+
+    for char in dict:
+        sorted_list.append({"char": char, "num": dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    
+    return sorted_list
